9bf24f50d06768fc53091e8b8d8887c712244bf1
AndreyKaBelka <andreykaov4innikov@yandex.ru>
2021-04-05 01:58:55 +0600

    message fix

--- utils/message.py
+++ utils/message.py
@@ -3,22 +3,29 @@ from vk_api.bot_longpoll import VkBotMessageEvent
 
 class Message:
     def __init__(self, from_id, text, from_title=None, chat_title=None):
-        self.from_id = from_id
+        self._from_id = from_id
         self.chat_title = chat_title
-        self.text = text
         self.from_title = from_title
+        self.text = text
 
     @staticmethod
     def from_vk(vk_event: VkBotMessageEvent):
         peer_id = vk_event.message.get('peer_id')
         text = vk_event.message.get('text')
-        return Message(peer_id, text)
+        from_title = vk_event.client_info.get('from_title')
+        chat_title = vk_event.message.get('chat_title')
+        return Message(peer_id, text, from_title, chat_title)
 
     @staticmethod
     def from_tg(tg_message):
         peer_id = tg_message.chat.id
         text = tg_message.chat.message
-        return Message(peer_id, text)
+        from_title = tg_message.chat.from_title
+        return Message(peer_id, text, from_title)
 
     def __str__(self):
-        return f'{{from_id = {self.from_id}, chat_title = {self.chat_title}, text = {self.text}, from_title = {self.chat_title}}}'
+        res = ''
+        for key, val in self.__dict__.items():
+            if val is not None and not key.startswith('_'):
+                res += f'{key} : {val}\n'
+        return res
