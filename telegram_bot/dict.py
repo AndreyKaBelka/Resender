cbaad8a3849e629d3636917f2d64ca1d52c5d2b6
AndreyKaBelka <andreykaov4innikov@yandex.ru>
2021-04-04 18:45:02 +0600

    add registration and utils

---
+++ telegram_bot/dict.py
@@ -0,0 +1,3 @@
+START_MESSAGE = "Welcome! Choose your action:"
+NEW_ACC_MESSAGE = "Your new acc id:\n{id}.\nPlease, connect it to your vk acc."
+EX_ACC_MESSAGE = "Please, reply to this message with your id"
